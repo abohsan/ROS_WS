69fa50242b1b382cdd8122a02039c50ace4219f7
Abdulmueen Alrashide <aalra021@fiu.edu>
2020-07-05 21:27:25 -0400

    Delete Arduino Projects

--- ESP32_WIFI_COW_Arduino/pub.h
+++
@@ -1,6 +0,0 @@
-//#include <sensor_msgs/JointState.h>
-//
-//ros::NodeHandle no;
-//sensor_msgs::JointState str_msg;
-//ros::Publisher chatter("Encoder", &str_msg);
-//char hello[13] = "hello world!";
